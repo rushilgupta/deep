3ac89269f4103fe5ddf11a5fb9b2519471c9cedf
Rushil Gupta <rushil.gupta@gmail.com>
2017-07-04 14:31:40 -0700

    the regularization

--- assignment-3.py
+++ assignment-3.py
@@ -38,84 +38,12 @@ print('Validation set', valid_dataset.shape, valid_labels.shape)
 print('Test set', test_dataset.shape, test_labels.shape)
 
 
-##########################
-# Gradient Descent LR
-##########################
-# With gradient descent training, even this much data is prohibitive.
-# Subset the training data for faster turnaround.
-train_subset = 10000
-
-graph = tf.Graph()
-with graph.as_default():
-
-  # Input data.
-  # Load the training, validation and test data into constants that are
-  # attached to the graph.
-  tf_train_dataset = tf.constant(train_dataset[:train_subset, :])
-  tf_train_labels = tf.constant(train_labels[:train_subset])
-  tf_valid_dataset = tf.constant(valid_dataset)
-  tf_test_dataset = tf.constant(test_dataset)
-
-  # Variables.
-  # These are the parameters that we are going to be training. The weight
-  # matrix will be initialized using random values following a (truncated)
-  # normal distribution (mean = 0 and equal variances). The biases get initialized to zero.
-  weights = tf.Variable(
-    tf.truncated_normal([image_size * image_size, num_labels]))
-  biases = tf.Variable(tf.zeros([num_labels]))
-
-  # Training computation.
-  # We multiply the inputs with the weight matrix, and add biases. We compute
-  # the softmax and cross-entropy (it's one operation in TensorFlow, because
-  # it's very common, and it can be optimized). We take the average of this
-  # cross-entropy across all training examples: that's our loss.
-  logits = tf.matmul(tf_train_dataset, weights) + biases
-  # Y' = WX + b, CrossEntropy(Softmax(Y`))
-  loss = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
-
-  # Optimizer.
-  # We are going to find the minimum of this loss using gradient descent.
-  optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
-
-  # Predictions for the training, validation, and test data.
-  # These are not part of training, but merely here so that we can report
-  # accuracy figures as we train.
-  train_prediction = tf.nn.softmax(logits)
-  valid_prediction = tf.nn.softmax(
-    tf.matmul(tf_valid_dataset, weights) + biases)
-  test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
-
-num_steps = 801
-
 def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
 
-with tf.Session(graph=graph) as session:
-  # This is a one-time operation which ensures the parameters get initialized as
-  # we described in the graph: random weights for the matrix, zeros for the
-  # biases.
-  tf.global_variables_initializer().run()
-  print('Initialized')
-  for step in range(num_steps):
-    # Run the computations. We tell .run() that we want to run the optimizer,
-    # and get the loss value and the training predictions returned as numpy
-    # arrays.
-    _, l, predictions = session.run([optimizer, loss, train_prediction])
-    if (step % 100 == 0):
-      print('Loss at step %d: %f' % (step, l))
-      print('Training accuracy: %.1f%%' % accuracy(
-        predictions, train_labels[:train_subset, :]))
-      # Calling .eval() on valid_prediction is basically like calling run(), but
-      # just to get that one numpy array. Note that it recomputes all its graph
-      # dependencies.
-      print('Validation accuracy: %.1f%%' % accuracy(
-        valid_prediction.eval(), valid_labels))
-  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
-
 ##########################
-# with STOCHASTIC LR (86.5%)
+# with STOCHASTIC LR (89%)
 ##########################
 batch_size = 128
 
@@ -136,15 +64,20 @@ with graph.as_default():
   biases = tf.Variable(tf.zeros([num_labels]))
 
   # Training computation.
-  logits = tf.matmul(tf_train_dataset, weights) + biases
-  loss = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+  model_scores = tf.matmul(tf_train_dataset, weights) + biases
 
+  loss1 = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=model_scores))
+
+  # L2 REGULARIZATION Penalty
+  l2_regularization = tf.nn.l2_loss(weights)
+  loss = tf.reduce_mean(loss1 + 0.001*l2_regularization)
+  #loss = loss1 + beta*l2_regularization
   # Optimizer.
   optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
   # Predictions for the training, validation, and test data.
-  train_prediction = tf.nn.softmax(logits)
+  train_prediction = tf.nn.softmax(model_scores)
   valid_prediction = tf.nn.softmax(
     tf.matmul(tf_valid_dataset, weights) + biases)
   test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
@@ -165,22 +98,23 @@ with tf.Session(graph=graph) as session:
     # The key of the dictionary is the placeholder node of the graph to be fed,
     # and the value is the numpy array to feed to it.
     feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
-    _, l, predictions = session.run(
-      [optimizer, loss, train_prediction], feed_dict=feed_dict)
+    _, l, predictions, l2 = session.run(
+      [optimizer, loss, train_prediction,l2_regularization], feed_dict=feed_dict)
     if (step % 500 == 0):
       print("Minibatch loss at step %d: %f" % (step, l))
+      print("Reg loss at step %d: %f" % (step, l2))
       print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
       print("Validation accuracy: %.1f%%" % accuracy(
         valid_prediction.eval(), valid_labels))
   print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
 
-
 ##########################
-# First Neural Network (89.4%)
+# Neural Network + L2 reg (93%)
 ##########################
 
 batch_size = 128
 hidden_nodes = 1024
+beta = 0.001
 
 graph = tf.Graph()
 with graph.as_default():
@@ -218,8 +152,12 @@ with graph.as_default():
   logits = three_layer_network(tf_train_dataset)
 
   # avg. cross entropy loss
-  loss = tf.reduce_mean(
+  loss1 = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+
+  l2_regularization_layer1 = tf.nn.l2_loss(layer1_weights)
+  l2_regularization_layer2 = tf.nn.l2_loss(layer2_weights)
+  loss = tf.reduce_mean(loss1 + beta*l2_regularization_layer1 + beta*l2_regularization_layer2)
 
   # Optimizer.
   optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
